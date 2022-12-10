05ea45eb9067b6391378999b6a9553bbfe1e0267
2211o2 <hana000all@gmail.com>
2022-12-10 10:11:06 +0800

    cleaning up

--- advent07.py
+++ advent07.py
@@ -1,7 +1,12 @@
 def main():
     with open('inputs/input07', 'r', encoding="utf-8") as f:
         lines = f.readlines()
-        
+    
+    directories = parse_input(lines)
+    part_one(directories)
+    part_two(directories)
+
+def parse_input(lines):
     directories = {}
     curr_dir = []
 
@@ -27,13 +32,15 @@ def main():
             if size.isnumeric():
                 for i in range(1, len(curr_dir) + 1):
                     directories[ '/'.join(curr_dir[:i]) ] += int(size)
+    
+    return directories
 
-    print(directories)
+def part_one(directories):
     print(sum([x for x in directories.values() if x <= 100_000]))
 
 def part_two(directories):
-    available = 70_000_000
-    need      = 30_000_000
+    available   = 70_000_000
+    needed      = 30_000_000
 
 if __name__ == '__main__':
     main()
